20c5042edff32099401a314768880625c7df5c92
Davey Newhall <newhall@bitwise.io>
2020-04-14 14:44:01 -0500

    Update SABRE_FAMILY_VERSION
    This updates the SABRE_FAMILY_VERSION constant to '0.5'. This was
    preventing Sabre transactions from being submitted successfully.
    
    Signed-off-by: Davey Newhall <newhall@bitwise.io>

--- src/sabre/index.ts
+++ src/sabre/index.ts
@@ -18,4 +18,4 @@ export { TransactionBuilder as SabreTransactionBuilder } from './transaction';
 // The Sawtooth Sabre transaction family name (sabre)
 export const SABRE_FAMILY_NAME = 'sabre';
 // The Sawtooth Sabre transaction family version (0.4)
-export const SABRE_FAMILY_VERSION = '0.4';
+export const SABRE_FAMILY_VERSION = '0.5';
